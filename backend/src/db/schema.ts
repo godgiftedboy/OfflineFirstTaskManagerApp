2e952e2002b3f57be3a21e28e37e89be9ca70fe9
godgiftedboy <rajprajapati12321@gmail.com>
2025-03-28 15:18:03 +0545

    chore: signup endpoint code - packages added- bcryptjs, dev- @types/bcryptjs, drizzle-kit,

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,7 +1,11 @@
 
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
+// Use Zod if:
 
+// You want to validate data before inserting it (e.g., checking for a valid email format).
+// You need custom error messages.
+// You're building an API and want to return user-friendly validation errors.
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull(),
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,51 @@
-import { Router } from "express";
+import bcryptjs from "bcryptjs";
+import { eq } from "drizzle-orm";
+import { Request, Response, Router } from "express";
+import { db } from "../db";
+import { NewUser, users } from "../db/schema";
 
 const authRouter = Router();
+
+interface SignUpBody{
+    name: string,
+    email: string,
+    password: string,
+};
+
+authRouter.post("/signup",
+    async (req: Request<{},{}, SignUpBody>, res: Response)=>{
+    try {
+        //get req body 
+        const {name, email, password} = req.body;
+        //check if user already exists
+        const existingUser =  await db.select().from(users).where(eq(users.email, email));
+
+        if(!existingUser.length){
+             res.status(400).json({"message":"User already exists"})
+             return
+        }
+        //create a new user and store it in db
+
+        const hashedPassword = await bcryptjs.hash(password,8);
+
+        const newUser : NewUser= {
+            name, //shorthand for name: name
+            email: email,
+            password: hashedPassword,
+        } 
+        //destructured the list to get only a user
+        //as it return the list of users.
+        const [user] = await db.insert(users).values(newUser).returning();
+
+        res.status(201).json(user);
+
+        
+    } catch (error) {
+        res.status(500).json({error:error});
+        
+    }
+})
+
 
 authRouter.get("/",(req,res)=>{
     res.send("hello from auht Router");
