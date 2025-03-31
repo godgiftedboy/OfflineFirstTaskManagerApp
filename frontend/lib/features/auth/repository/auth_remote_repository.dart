5f391fd15df67e334f6a44c13b24fc788797e094
godgiftedboy <rajprajapati12321@gmail.com>
2025-03-31 13:09:36 +0545

    chore: auth state persisted using shared prefs

--- frontend/lib/features/auth/repository/auth_remote_repository.dart
+++ frontend/lib/features/auth/repository/auth_remote_repository.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
 import 'package:frontend/core/constants/constants.dart';
+import 'package:frontend/core/services/sp_service.dart';
 import 'package:frontend/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class AuthRemoteRepository {
+  final spService = SpService();
   Future<UserModel> signUp({
     required String name,
     required String email,
@@ -61,5 +63,48 @@ class AuthRemoteRepository {
     } catch (e) {
       throw e.toString();
     }
+  }
+
+  Future<UserModel?> getUserData() async {
+    try {
+      final token = await spService.getToken();
+      if (token == null) {
+        return null;
+      }
+
+      final res = await http.post(
+        Uri.parse(
+          '${Constants.backendUri}/auth/isTokenValid',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+      );
+
+      if (res.statusCode != 200 || jsonDecode(res.body) == false) {
+        return null;
+      }
+
+      final userResponse = await http.get(
+        Uri.parse(
+          '${Constants.backendUri}/auth',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+      );
+
+      if (userResponse.statusCode != 200) {
+        throw jsonDecode(userResponse.body)['error'];
+      }
+      return UserModel.fromJson(userResponse.body);
+    } catch (e) {
+      // final user = await authLocalRepository.getUser();
+      // print(user);
+      // return user;
+    }
+    return null;
   }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/features/auth/cubit/auth_cubit.dart';
-import 'package:frontend/features/auth/pages/login_page.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+import 'package:frontend/features/home/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+    providers: [BlocProvider(create: (_) => AuthCubit())],
+    child: const MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthCubit>().getUserData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => AuthCubit())],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const LoginPage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoggedIn) {
+            return const HomePage();
+          }
+          return const SignupPage();
+        },
       ),
     );
   }
