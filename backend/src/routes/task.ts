a6e665f4a036350ce300a18dde4d95a3933dee81
godgiftedboy <rajprajapati12321@gmail.com>
2025-03-31 15:45:53 +0545

    chore: added task fetch and delete endpoint

--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { Router } from "express";
 import { db } from "../db";
 import { NewTask, tasks } from "../db/schema";
@@ -21,5 +22,31 @@ taskRouter.post('/',auth, async(req: AuthRequest,res)=>{
     }
 
 })
+
+
+taskRouter.get('/',auth, async(req: AuthRequest,res)=>{
+    try {
+        const allTasks = await db.select().from(tasks).where(eq(tasks.uid,req.user!));
+
+        res.status(201).json(allTasks);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:error});  
+    }
+})
+
+taskRouter.delete('/',auth, async(req: AuthRequest,res)=>{
+    try {
+        const {taskId}:{taskId: string} = req.body;
+
+        await db.delete(tasks).where(eq(tasks.id,taskId));
+
+        res.status(201).json(true);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:error});  
+    }
+})
+
 
 export default taskRouter;
